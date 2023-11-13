416b6612710b065021a353938ed90dca909af31b
Karma243162 <mohammed.tahri24@gmail.com>
2023-11-13 11:47:26 +0100

    Ajout de l ajout quizz admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Quiz;
+use App\Form\AddQuizzType;
+
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(Request $request,EntityManagerInterface $entityManagerInterface): Response
+    {
+
+        $addQuizz = new Quiz();
+
+        $form = $this->createForm(AddQuizzType::class, $addQuizz);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($addQuizz);
+            $entityManagerInterface->flush();
+              return $this->redirectToRoute('index'); 
+          }
+
+        return $this->render('admin/addQuizz.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView()
+        ]);
+    }
+}
